2b44c5a9d74711c5f2465c93eb471de10a9e82f0
salss33 <hallosalsa03@gmail.com>
2023-04-03 14:37:27 +0700

    memanggil data Mahasiswa 1 dan 2

--- Activity Structure_189/Activity Structure_189.cpp
+++ Activity Structure_189/Activity Structure_189.cpp
@@ -8,3 +8,13 @@ struct Mahasiswa {
 	string alamat;
 	int umur;
 };
+
+int main()
+{
+	Mahasiswa mhs1, mhs2;
+
+	mhs1.nim = "2021014011";
+	mhs1.nama = "tasya";
+	mhs1.alamat = "semarang";
+	mhs1.umur = 20;
+}
