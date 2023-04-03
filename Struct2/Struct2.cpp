45553ad2e454b446865a012d420eed8efba9e5c1
salss33 <hallosalsa03@gmail.com>
2023-04-03 15:48:22 +0700

    Memasukkan dan menampilkan NIM, Nama , Alamat, Desa, Kota, Umur

--- Struct2/Struct2.cpp
+++ Struct2/Struct2.cpp
@@ -5,14 +5,39 @@ using namespace std;
 struct AlamatDetail
 {
 	string desa;
-	string data;
+	string kota;
+
+
+};
+
+struct Mahasiswa {
+	string nim;
+	string nama;
+	AlamatDetail alamat;
+	int umur;
+};
+
+int main() {
+
+	Mahasiswa mhs;
+
+	cout << "Masukkan nim: ";
+	cin >> mhs.nim;
+	cout << "Masukkan nama : ";
+	cin >> mhs.nama;
+	cout << "Alamat : " << endl;
+	cout << "\tNama desa : ";
+	cin >> mhs.alamat.desa;
+	cout << "\tNama kota : ";
+	cin >> mhs.alamat.kota;
+	cout << "Masukkan umur : ";
+	cin >> mhs.umur;
+
+	cout << "\nNIm : " << mhs.nim;
+	cout << "\nnama : " << mhs.nama;
+	cout << "\ndesa : " << mhs.alamat.desa;
+	cout << "\nkota : " << mhs.alamat.kota;
+
 
 
 }
-
-
-
-
-
-
-
